80e9c96405f8af7d1a068e0cee5f6ea11923006b
Camilo Calderon <cam.calderono@duocuc.cl>
2026-06-30 17:17:12 -0400

    cambios

--- src/services/logistics.service.ts
+++ src/services/logistics.service.ts
@@ -21,7 +21,7 @@ export async function listarTransferencias(): Promise<TransferenciaResponse[]> {
   const res = await apiClient.get<TransferenciaResponse[]>('/transferencias');
   return res.data;
 }
-
+//EXPORTA
 export async function crearTransferencia(
   data: CrearTransferenciaRequest
 ): Promise<TransferenciaResponse> {
